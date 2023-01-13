a0484c1176cb277aa7f82ded772ee3c05b912f93
Bruno Barreto <106797767+TheKods@users.noreply.github.com>
2023-01-12 21:17:40 -0300

    #Python

---
+++ python35.py
@@ -0,0 +1,14 @@
+'''
+Repetição
+while(enquanto)
+executa uma ação enquanto uma condição for verdadeira
+loop infinito -> laço infinito de repetição do código
+'''
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    contador = contador + 1
+
+    
+print('Acabou')
