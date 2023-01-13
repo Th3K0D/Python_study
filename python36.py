cb05dbf3bf94ee12a68bda389df16ed5a5ca006b
Bruno Barreto <106797767+TheKods@users.noreply.github.com>
2023-01-13 16:52:40 -0300

    #Python
    Just some scripts where I'm exercising my daily studies in python

---
+++ python36.py
@@ -0,0 +1,19 @@
+'''
+Operadores de atribuição
+= += -= *= /= //= **= %= 
+'''
+
+contador = 2
+
+# while contador <= 10:
+#     print(contador)
+#     contador = contador + 1
+
+    
+# print('Acabou')
+
+contador **= 10
+
+
+
+print(contador)
