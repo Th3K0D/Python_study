52d21333e91ff643cf93236cb545340631e98e97
Bruno Barreto <106797767+TheKods@users.noreply.github.com>
2023-01-20 08:39:34 -0300

    #Python
    Just some scripts where I'm exercising my daily studies in python

---
+++ python41.py
@@ -0,0 +1,18 @@
+'''
+ While/Else
+'''
+
+string = 'valor qualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+    
+    if letra == '':
+        break
+    
+    print(letra)
+    i+= 1
+else:
+    print('O else foi executado')
+print('Fora do While')
