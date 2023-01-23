5771aea6037c498c885de1f7e40bfe9b88ff4291
Bruno Barreto <106797767+TheKods@users.noreply.github.com>
2023-01-23 15:35:52 -0300

    #Python
    Just some scripts where I'm exercising my daily studies in python

---
+++ python44.py
@@ -0,0 +1,11 @@
+""" 
+For + Range
+range -> range(start, stop, step)
+"""
+# da para calcular a tabuada , só colocar o limite minimo e o maximo e o multiplicador 
+numeros = range(0, 100, 8)
+
+for numero in numeros:
+    print(numero)
+    
+    
